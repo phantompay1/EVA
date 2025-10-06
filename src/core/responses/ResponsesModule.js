8726f9b19bd4577d50758ae842ab735abedee5e0
phantompay1 <phantompaywallet@gmail.com>
2025-10-06 14:52:23 +0300

    Updated ResponsesModule.js

--- src/core/responses/ResponsesModule.js
+++ src/core/responses/ResponsesModule.js
@@ -4,7 +4,29 @@
  * Part of EVA's Foundational Knowledge Modules
  */
 
-const EventEmitter = require('events');
+class EventEmitter {
+    constructor() {
+        this.events = {};
+    }
+
+    on(event, listener) {
+        if (!this.events[event]) {
+            this.events[event] = [];
+        }
+        this.events[event].push(listener);
+    }
+
+    emit(event, ...args) {
+        if (this.events[event]) {
+            this.events[event].forEach(listener => listener(...args));
+        }
+    }
+
+    removeListener(event, listenerToRemove) {
+        if (!this.events[event]) return;
+        this.events[event] = this.events[event].filter(listener => listener !== listenerToRemove);
+    }
+}
 
 /**
  * Text Response Generator
